c38829c48d6a74e5f3570709bc8cd1a594f0c8c3
luiscelanomrb <luiscelano@twowheelstogo.com>
2021-07-14 19:43:30 -0600

    Adding customTrailing to addressRadio list

--- components/AddressList/AddressList.js
+++ components/AddressList/AddressList.js
@@ -4,12 +4,37 @@ import { withComponents } from "@reactioncommerce/components-context";
 import styled from "styled-components";
 import { Menu, MenuItem, IconButton } from "@material-ui/core";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
+import AddIcon from "@material-ui/icons/Add";
 const Items = styled.div`
     display: flex;
     flex-direction: column;
     gap: 15px;
     justify-content: flex-start;
-    align-items: flex-start;
+    align-items: flex-end;
+`;
+const CustomRoundedButton = styled.div`
+    width: auto;
+    text-align: center;
+    min-height: 34px;
+    display: flex;
+    flex-direction: row;
+    gap: 10px;
+    justify-content: flex-end;
+    align-items: center;
+    background: #F4F1F1;
+    border-radius: 14px;
+    padding-left: 10px;
+    padding-right: 10px;
+    color:black;
+    cursor: pointer;
+    &:hover{
+        color:white;
+        background: #DCDCDC
+    }
+`;
+const CustomButtonText = styled.div`
+    font-size: 16px;
+    font-height: 400;
 `;
 const Controls = (props) => {
     const {menuOpen,handleClose,handleOpen} = props;
@@ -69,6 +94,10 @@ class AddressList extends Component{
                     handleChange={()=>{}}
                     />
                 ))}
+                <CustomRoundedButton>
+                    <CustomButtonText>{"Agregar otra dirección"}</CustomButtonText>
+                    <AddIcon/>
+                </CustomRoundedButton>
             </Items>
         );
     }
