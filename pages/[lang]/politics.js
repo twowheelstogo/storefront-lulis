3ccddfc720511ba3f5bd42059f4508d561b049ad
luiscelanomrb <luiscelano@twowheelstogo.com>
2021-08-06 12:51:16 -0600

    adding payment politics and linking all footer items

--- pages/[lang]/politics.js
+++ pages/[lang]/politics.js
@@ -44,16 +44,15 @@ class PoliticsAndDevolutions extends React.Component{
 					</Grid>
 					<Grid item>
 						<h4 className={subtitle} style={{color:"#0095b3"}}>DEVOLUCIÓN/ REEMBOLSO</h4>
-						<p>Al tratarse de comida a domicilio, la política de devolución se basa en la resolución de conflictos en el momento de la recepción del pedido. Si existe inconformidad en cuanto a la calidad del producto, contactarse directamente por medio de mensaje directo por Instagram, Whatsapp, mensaje de Facebook, enviando correo por medio de la página web.</p>
+						<p>Al tratarse de comida a domicilio, la política de devolución se basa en la resolución de conflictos en el momento de la recepción del pedido. Si existe inconformidad en cuanto a la calidad del producto o de la orden, contactarse directamente por correo electrónico a luiscelano@twowheelstogo.com</p>
 						<br></br>
 						<blockquote>
 							<p>1. El cliente inspeccionara el producto inmediatamente tras su llegada al destino pactado, con el fin de determinar si ha sido dañado durante el transporte.</p>
-							<p>2. Si se encontrara un defecto de calidad, el Cliente deberá poner el producto a disposición del Vendedor para ser evaluado. La forma para proceder será contactando a LULIS en un plazo por medio de mensaje directo por Instagram, Whatsapp, mensaje de Facebook, enviando correo por medio de la página web adjuntando fotografías, descripción de incidencia y código de envío. El Vendedor podrá optar entre:</p>
+							<p>Si se encontrara un defecto de calidad, el Cliente deberá poner el producto a disposición del Vendedor para ser evaluado. La forma para proceder será contactando a LULIS por medio de correo electrónico, adjuntando fotografías, descripción de incidencia y código de envío. El  Vendedor podrá optar entre:</p>
 						</blockquote>
 						<blockquote>
 							<p>a. Sustitución del producto</p>
 							<p>b. Reembolso por valor del producto</p>
-							<p>c. Reducir el precio en proporción al defecto del producto</p>
 						</blockquote>
 						<br></br>
 						<p>En cualquiera de los escenarios, el Vendedor asumirá los gastos de transporte o sustitución del producto.</p>
