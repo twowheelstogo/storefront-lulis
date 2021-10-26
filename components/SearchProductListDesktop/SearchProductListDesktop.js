61cd160d45eea1d688ed92e0ed7a0cf04edeb79e
luiscelanomrb <luiscelano@twowheelstogo.com>
2021-10-26 11:29:24 -0600

    feat: upgrade search view

--- components/SearchProductListDesktop/SearchProductListDesktop.js
+++ components/SearchProductListDesktop/SearchProductListDesktop.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from "react";
-import {withComponents} from "@reactioncommerce/components-context";
+import { withComponents } from "@reactioncommerce/components-context";
 import styled from "styled-components";
 import PropTypes from "prop-types";
 import { CustomPropTypes } from "@reactioncommerce/components/utils";
@@ -8,48 +8,50 @@ const Items = styled.div`
     flex-direction:column;
     gap: 20px;
 `;
-const CustomText2= styled.div`
+const CustomText2 = styled.div`
     font-size: 24px;
     color: #B8BCCA;
 `;
-class SearchProductListDesktop extends Component{
-    static propTypes = {
-    	items:PropTypes.array.isRequired,
-    	components: PropTypes.shape({
-    		HorizontalProductCard: CustomPropTypes.component.isRequired
-    	})
-    }
-    render(){
-    	const {
-    		items,
-    		components:{HorizontalProductCard},
-    		addItemsToCart,
-    		onChangeCartItemsQuantity,
-    		cart,
-    		currencyCode,
-    		uiStore
-    	} = this.props;
-    	return(
-    		<Items>
-    			<CustomText2>{`${items.length} coincidencia${items.length==1?"":"s"}`}</CustomText2>
-    			{(items||[]).map((item)=>{
-    				const productInCart = (cart?.items||[]).find((cartItem)=>cartItem.productSlug==item.slug);
-    				return{
-    					...item,
-    					cartItem:productInCart
-    				};
-    			})
-    				.map((item)=>(
-    					<HorizontalProductCard
-    						currencyCode = {currencyCode}
-    						product = {item}
-    						uiStore={uiStore}
-    						addItemsToCart={addItemsToCart}
-    						onChangeCartItemsQuantity={onChangeCartItemsQuantity}
-    					/>
-    				))}
-    		</Items>
-    	);
-    }
+class SearchProductListDesktop extends Component {
+	static propTypes = {
+		items: PropTypes.array.isRequired,
+		components: PropTypes.shape({
+			HorizontalProductCard: CustomPropTypes.component.isRequired
+		})
+	}
+	render() {
+		const {
+			items,
+			components: { HorizontalProductCard },
+			addItemsToCart,
+			onChangeCartItemsQuantity,
+			cart,
+			addOrCreateCartLoading,
+			currencyCode,
+			uiStore
+		} = this.props;
+		return (
+			<Items>
+				<CustomText2>{`${items.length} coincidencia${items.length == 1 ? "" : "s"}`}</CustomText2>
+				{(items || []).map((item) => {
+					const productInCart = (cart?.items || []).find((cartItem) => cartItem.productSlug == item.slug);
+					return {
+						...item,
+						cartItem: productInCart
+					};
+				})
+					.map((item) => (
+						<HorizontalProductCard
+							addOrCreateCartLoading={addOrCreateCartLoading}
+							currencyCode={currencyCode}
+							product={item}
+							uiStore={uiStore}
+							addItemsToCart={addItemsToCart}
+							onChangeCartItemsQuantity={onChangeCartItemsQuantity}
+						/>
+					))}
+			</Items>
+		);
+	}
 }
 export default withComponents(SearchProductListDesktop);
